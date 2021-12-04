c192fee50fe5d91225195fc3b63bdaf3500c528d
Pranjal Tripathi <pranjalvkt.srmcem@gmail.com>
2021-12-04 17:45:17 +0530

    tree command fix

--- commands/tree.js
+++ commands/tree.js
@@ -1,36 +1,38 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-function treeFn(dirPath) {
-    // console.log('Tree function');
-    // ├──
-    if(dirPath == undefined) {
-        console.log("Please enter a valid Path");
-    } else {
-        let doesExist = fs.existsSync(dirPath)
-        if(doesExist) {
-            treeHelper(dirPath + " ");
-        }
+
+function treeFn(dirpath) {
+  if (dirpath == undefined) {
+    console.log("Please enter a valid Path");
+  } else {
+    let doesExist = fs.existsSync(dirpath);
+    if (doesExist) {
+      treeHelper(dirpath, " ");
     }
+  }
 }
 
-function treeHelper(targetPath, intend) {
-    let isFile = fs.lstatSync(targetPath).isFile();
-    if(isFile) {
-        let fileName = path.basename(targetPath);
-        console.log(intend + "├──" + fileName);
-    } else {
-        let dirName = path.basename(targetPath);
-        console.log(intend + "└──" + dirName);
+function treeHelper(targetPath, indent) {
+  let isFile = fs.lstatSync(targetPath).isFile();
 
-        let children = fs.readFileSync(targetPath);
+  if (isFile == true) {
+    let fileName = path.basename(targetPath);
+    console.log(indent + "├──" + fileName);
+  } else {
+    let dirName = path.basename(targetPath);
+    console.log(indent + "└──" + dirName);
 
-        for(let i = 0; i < children.length; i++) {
-            let childPath = path.join(targetPath, children[i]);
-            treeHelper(childPath, intend + '\t');
-        }
+    let children = fs.readdirSync(targetPath);
+
+    for (let i = 0; i < children.length; i++) {
+      let childPath = path.join(targetPath, children[i]);
+      treeHelper(childPath, indent + "\t");
     }
+  }
 }
-module.exports = {
-    treeKey: treeFn
+
+
+module.exports={
+       treeKey : treeFn
 }
